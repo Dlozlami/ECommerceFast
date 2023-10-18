f7fa77f7d5d6c2c3f04f3577fe17b82984a521bf
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-10-18 14:05:53 +0200

    CHORE: Customers

---
+++ screens/Customers/customers.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Customers() {
+  return (
+    <View>
+      <Text>customers</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
